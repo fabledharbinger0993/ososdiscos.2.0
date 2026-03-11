bd348479640fcb80ef381d6550a932bf376b3779
fabledharbinger0993 <fabledharbinger@gmail.com>
2026-03-11 03:23:22 -0700

    finished

--- frontend/pages/admin/index.tsx
+++ frontend/pages/admin/index.tsx
@@ -1,15 +1,14 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
 
-import Header from "../components/Header"
-import Hero from "../components/HeroCarousel"
-import MovieReel from "../components/MovieReel"
-import SoundSection from "../components/SoundSection"
-import PictureCarousel from "../components/PictureCarousel"
-import EventFlyers from "../components/EventFlyers"
-import Bio from "../components/BioPanel"
-import EventCalendar from "../components/EventCalendar"
-
+import Header from "../../components/Header"
+import Hero from "../../components/HeroCarousel"
+import MovieReel from "../../components/MovieReel"
+import SoundSection from "../../components/SoundSection"
+import PictureCarousel from "../../components/PictureCarousel"
+import EventFlyers from "../../components/EventFlyers"
+import Bio from "../../components/BioPanel"
+import EventCalendar from "../../components/EventCalendar"
 const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://ososdiscoscom-production.up.railway.app"
 
 const DEFAULT_LAYOUT = [
