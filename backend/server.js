7ee14b83192f46a4b9179c6a62688d20222ab4e0
FabledHarbinger <fabledharbinger@gmail.com>
2026-03-14 03:30:20 -0400

    Import User model in server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import "dotenv/config"
 import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
+import User from "./models/User.js"
 
 // ── Route imports ─────────────────────────────────────────────────────────────
 import loginRouter    from "./routes/login.js"
